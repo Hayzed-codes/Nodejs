5f42cb39c094c37599b55e0d6e2d77bb44d06b88
Hayzed-codes <raheemhorlah@gmail.com>
2024-05-13 11:55:31 +0100

    Express-routers

---
+++ express_routers-main/routes/api/employee.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router(); 
+const data = {};
+data.employees = require("../../data/employees.json")
+
+const path = require("path");
+
+//  first method.
+// router.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "..", "..", "data", "employees.json"))
+// })
+
+router
+    .route("/")
+    .get((req, res) => {
+        res.json(data.employees)
+    })
+
+module.exports = router;
